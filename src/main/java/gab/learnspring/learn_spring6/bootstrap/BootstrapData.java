670eecf08185ad466b7fd3394e50a5526b459a57
Gabriel Teixeira Charles da Silva <charlesgabriel102@gmail.com>
2025-01-23 19:56:57 -0300

    New Author added: John Doe

--- src/main/java/gab/learnspring/learn_spring6/bootstrap/BootstrapData.java
+++ src/main/java/gab/learnspring/learn_spring6/bootstrap/BootstrapData.java
@@ -25,33 +25,38 @@ public class BootstrapData implements CommandLineRunner {
         eric.setFirstName("Eric");
         eric.setLastName("Evans");
 
-        // Author john = new Author();
-        // john.setFirstName("John");
-        // john.setLastName("Doe");
-
-        Book ddd = new Book();
-        ddd.setTitle("Domain Driven Design");
-        ddd.setIsbn("1234567890");
-
-        Author ericSaved = authorRepository.save(eric);
-        Book dddSaved = bookRepository.save(ddd);
+        Author john = new Author();
+        john.setFirstName("John");
+        john.setLastName("Doe");
 
         Author rod = new Author();
         rod.setFirstName("Rod");
         rod.setLastName("Johnson");
 
+        Book ddd = new Book();
+        ddd.setTitle("Domain Driven Design");
+        ddd.setIsbn("1234567890");
+
         Book noEJB = new Book();
         noEJB.setTitle("J2EE Development without EJB");
         noEJB.setIsbn("3241221");
+
+
+        Author ericSaved = authorRepository.save(eric);
+        Book dddSaved = bookRepository.save(ddd);
+
+        Author johnSaved = authorRepository.save(john);
 
         Author rodSaved = authorRepository.save(rod);
         Book noEJBSaved = bookRepository.save(noEJB);
 
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
+        johnSaved.getBooks().add(noEJBSaved);
 
         authorRepository.save(ericSaved);
         authorRepository.save(rodSaved);
+        authorRepository.save(johnSaved);
 
         System.out.println("In BootstrapData");
         System.out.println("Authors count: " + authorRepository.count());
