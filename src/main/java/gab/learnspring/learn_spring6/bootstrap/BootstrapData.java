94778b7983e3e0532d7b018260ec74f62f56d4ee
Gabriel Teixeira Charles da Silva <charlesgabriel102@gmail.com>
2025-01-24 16:38:08 -0300

    Creating a new Publisher, Code comments added

--- src/main/java/gab/learnspring/learn_spring6/bootstrap/BootstrapData.java
+++ src/main/java/gab/learnspring/learn_spring6/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package gab.learnspring.learn_spring6.bootstrap;
 
 import gab.learnspring.learn_spring6.domain.Author;
 import gab.learnspring.learn_spring6.domain.Book;
+import gab.learnspring.learn_spring6.domain.Publisher;
 import gab.learnspring.learn_spring6.repositories.AuthorRepository;
 import gab.learnspring.learn_spring6.repositories.BookRepository;
+import gab.learnspring.learn_spring6.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,15 +14,21 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        /*
+         * Setting all the authors
+         *
+         */
         Author eric = new Author();
         eric.setFirstName("Eric");
         eric.setLastName("Evans");
@@ -33,6 +41,22 @@ public class BootstrapData implements CommandLineRunner {
         rod.setFirstName("Rod");
         rod.setLastName("Johnson");
 
+        /*
+         * Creating a new publisher
+         *
+         */
+        Publisher Oreilly = new Publisher();
+        Oreilly.setPublisherName("O'Reilly");
+        Oreilly.setAddress("123 Main St.");
+        Oreilly.setCity("Any town");
+        Oreilly.setState("NY");
+        Oreilly.setZip("12345");
+        Publisher savedPublisher = publisherRepository.save(Oreilly);
+
+        /*
+         * Creating new Books
+         *
+         */
         Book ddd = new Book();
         ddd.setTitle("Domain Driven Design");
         ddd.setIsbn("1234567890");
@@ -41,7 +65,18 @@ public class BootstrapData implements CommandLineRunner {
         noEJB.setTitle("J2EE Development without EJB");
         noEJB.setIsbn("3241221");
 
+        /*
+        * Saving books in a publisher
+        *
+        */
+        ddd.setPublisher(savedPublisher);
+        noEJB.setPublisher(savedPublisher);
 
+
+        /*
+         * Saving everything in repositories
+         *
+         */
         Author ericSaved = authorRepository.save(eric);
         Book dddSaved = bookRepository.save(ddd);
 
@@ -50,16 +85,33 @@ public class BootstrapData implements CommandLineRunner {
         Author rodSaved = authorRepository.save(rod);
         Book noEJBSaved = bookRepository.save(noEJB);
 
+
+        /*
+        * Relating/Adding the books to their respective authors
+        *
+        * */
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
         johnSaved.getBooks().add(noEJBSaved);
 
+
+        /*
+        * Updating the authors in the repository
+        *
+        * */
         authorRepository.save(ericSaved);
         authorRepository.save(rodSaved);
         authorRepository.save(johnSaved);
 
+        /*
+         * Showing some info
+         *
+         * */
         System.out.println("In BootstrapData");
         System.out.println("Authors count: " + authorRepository.count());
         System.out.println("Books count: " + bookRepository.count());
+        System.out.println("Publishers count: " + publisherRepository.count());
+        System.out.println("---------------------------------------");
+
     }
 }
