bd28a480cbfe64984502b4e804350260908ebe8d
Gabriel Teixeira Charles da Silva <charlesgabriel102@gmail.com>
2025-01-24 16:36:56 -0300

    Book Entity Updated: add a publisher

--- src/main/java/gab/learnspring/learn_spring6/domain/Book.java
+++ src/main/java/gab/learnspring/learn_spring6/domain/Book.java
@@ -20,12 +20,23 @@ public class Book {
             inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
+    @ManyToOne
+    private Publisher publisher;
+
     public Set<Author> getAuthors() {
         return authors;
     }
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     public Long getId() {
@@ -58,7 +69,8 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
+                ", authors=" + authors + '\'' +
+                ", publisher=" + publisher +
                 '}';
     }
 
